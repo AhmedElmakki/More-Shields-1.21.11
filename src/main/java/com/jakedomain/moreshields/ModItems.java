c91a6f0aa9439c0ec476583b96c0c439f72c156e
Ahmed Elmakki <ahmed.hazelnut22@gmail.com>
2026-02-26 20:31:57 +0200

    mod items added

---
+++ src/main/java/com/jakedomain/moreshields/ModItems.java
@@ -0,0 +1,44 @@
+package com.jakedomain.moreshields;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ShieldItem;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems{
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems("moreshields");
+
+    public static final DeferredItem<ShieldItem> NETHERITE_SHIELD = ITEMS.registerItem(
+            "netherite_shield",
+            ShieldItem::new,
+            props -> props.durability(828).fireResistant()
+    );
+
+    public static final DeferredItem<ShieldItem> DIAMOND_SHIELD = ITEMS.registerItem(
+            "diamond_shield",
+            ShieldItem::new,
+            props -> props.durability(739)
+    );
+
+    public static final DeferredItem<ShieldItem> GOLD_SHIELD = ITEMS.registerItem(
+            "gold_shield",
+            ShieldItem::new,
+            props -> props.durability(156)
+    );
+
+    public static final DeferredItem<ShieldItem> IRON_SHIELD = ITEMS.registerItem(
+            "iron_shield",
+            ShieldItem::new,
+            props -> props.durability(336)
+    );
+
+    public static final DeferredItem<ShieldItem> COPPER_SHIELD = ITEMS.registerItem(
+            "copper_shield",
+            ShieldItem::new,
+            props -> props.durability(246)
+    );
+
+
+}
+
--- src/main/java/com/jakedomain/moreshields/MoreShields.java
+++ src/main/java/com/jakedomain/moreshields/MoreShields.java
@@ -1,117 +1,67 @@
-package com.jakedomain.moreshields;
-
-import org.slf4j.Logger;
+package com.jakedomain.moreshields; // Make sure this matches your actual package!
 
 import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
+
+
 @Mod(MoreShields.MODID)
 public class MoreShields {
+
     // Define mod id in a common place for everything to reference
     public static final String MODID = "moreshields";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "moreshields" namespace
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "moreshields" namespace
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "moreshields" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new Block with the id "moreshields:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", p -> p.mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "moreshields:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
-
-    // Creates a new food item with the id "moreshields:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", p -> p.food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
-
-    // Creates a creative tab with the id "moreshields:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.moreshields")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public MoreShields(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
+        // 1. Ignite your custom items registry!
+        ModItems.ITEMS.register(modEventBus);
+
+        // Placeholder: Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (MoreShields) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
+        // Listen for the Creative Tab event so we can put your shield in the menu
         modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        // Placeholder: Register ourselves for server and other game events
+        NeoForge.EVENT_BUS.register(this);
+
+        // Placeholder: Register config (Assuming you kept the Config class from the template)
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
-            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-        }
-
-        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
-
-        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Placeholder for future setup code
+        LOGGER.info("More Shields common setup is running!");
     }
 
-    // Add the example block item to the building blocks tab
+    // Add your custom items to the vanilla creative tabs
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        // Check if the current tab loading is the Combat tab
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            // Add our Diamond Shield to the tab!
+            event.accept(ModItems.NETHERITE_SHIELD);
+            event.accept(ModItems.DIAMOND_SHIELD);
+            event.accept(ModItems.GOLD_SHIELD);
+            event.accept(ModItems.IRON_SHIELD);
+            event.accept(ModItems.COPPER_SHIELD);
         }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    // Placeholder: Let the Event Bus discover methods to call on server start
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        LOGGER.info("More Shields says hello from the server!");
     }
 }
